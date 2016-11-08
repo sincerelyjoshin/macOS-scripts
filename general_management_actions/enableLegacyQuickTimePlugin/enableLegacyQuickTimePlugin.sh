cb82db7ef89047ea50396fb13b0d6a329d7c5d12
Josh Hovinetz <sincerelyjoshin@users.noreply.github.com>
2016-11-08 10:17:46 -0500

    Update enableLegacyQuickTimePlugin.sh

--- general_management_actions/enableLegacyQuickTimePlugin/enableLegacyQuickTimePlugin.sh
+++ general_management_actions/enableLegacyQuickTimePlugin/enableLegacyQuickTimePlugin.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 /bin/cp -rf /Library/Internet\ Plug-Ins/Disabled\ Plug-Ins/nsIQTScriptablePlugin.xpt /Library/Internet\ Plug-Ins/Disabled\ Plug-Ins/QuickTime\ Plugin.plugin /Library/Internet\ Plug-Ins/
+
+exit 0
