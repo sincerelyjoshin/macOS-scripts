6b13d1b868b695714d7235412f7250ad8c7c0d07
Josh Hovinetz <hovinetz@Hovinetzs-MacBook-Pro.local>
2016-11-14 21:36:22 -0500

    managing automatic spelling correction
    commit 2 new scripts to enable/disable automatic spelling correction

---
+++ general_management_actions/disableAutomaticSpellingCorrection/disableAutomaticSpellingCorrection.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+exit 0
---
+++ general_management_actions/enableAutomaticSpellingCorrection/enableAutomaticSpellingCorrection.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool true
+
+exit 0
