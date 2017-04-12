44ff1bac20588bdf38a65ced2a630611c97f9544
Josh Hovinetz <josh.hovinetz@gmail.com>
2017-04-12 15:30:38 -0400

    unloadLightspeedMobileFilter.py
    rewrite in python

---
+++ general_management_actions/unloadLightspeedMobileFilter/unloadLightspeedMobileFilter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import os
+import shutil
+import sys
+
+os.system("/usr/local/bin/mobilefilter -unload")
+os.system("kextunload /System/Library/Extensions/MobileFilterKext.kext")
+
+sys.exit(0)
