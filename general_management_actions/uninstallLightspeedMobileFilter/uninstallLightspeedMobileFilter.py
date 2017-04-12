dd8628aba2b4db9e32b99614c3628aa86ec00641
Josh Hovinetz <josh.hovinetz@gmail.com>
2017-04-12 13:39:55 -0400

    new script
    uninstallLightspeedMobileFilter in python

---
+++ general_management_actions/uninstallLightspeedMobileFilter/uninstallLightspeedMobileFilter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os
+import shutil
+import sys
+
+os.system("/usr/local/bin/mobilefilter -unload")
+
+if os.path.exists("/usr/local/bin/mobilefilter"):
+    os.remove("/usr/local/bin/mobilefilter")
+else:
+    print("/usr/local/bin/mobilefilter does not exist")
+
+if os.path.exists("/Library/LaunchDaemons/com.lightspeedsystems.mobilefilter.plist"):
+    os.remove("/Library/LaunchDaemons/com.lightspeedsystems.mobilefilter.plist")
+else:
+    print("/Library/LaunchDaemons/com.lightspeedsystems.mobilefilter.plist does not exist")
+
+os.system("kextunload /System/Library/Extensions/MobileFilterKext.kext")
+
+if os.path.exists("/System/Library/Extensions/MobileFilterKext.kext"):
+    shutil.rmtree("/System/Library/Extensions/MobileFilterKext.kext")
+else:
+    print("/System/Library/Extensions/MobileFilterKext.kext does not exist")
+
+sys.exit(0)
