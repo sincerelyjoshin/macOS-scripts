3737374aee406ce7ac63ad602b8989b4893bb759
Josh Hovinetz <josh.hovinetz@gmail.com>
2017-04-13 09:07:47 -0400

    uninstallLightspeedMobileFilter
    update conditionals

--- general_management_actions/uninstallLightspeedMobileFilter/uninstallLightspeedMobileFilter.py
+++ general_management_actions/uninstallLightspeedMobileFilter/uninstallLightspeedMobileFilter.py
@@ -4,9 +4,8 @@ import os
 import shutil
 import sys
 
-os.system("/usr/local/bin/mobilefilter -unload")
-
 if os.path.exists("/usr/local/bin/mobilefilter"):
+    os.system("/usr/local/bin/mobilefilter -unload")
     os.remove("/usr/local/bin/mobilefilter")
 else:
     print("/usr/local/bin/mobilefilter does not exist")
@@ -16,9 +15,8 @@ if os.path.exists("/Library/LaunchDaemons/com.lightspeedsystems.mobilefilter.pli
 else:
     print("/Library/LaunchDaemons/com.lightspeedsystems.mobilefilter.plist does not exist")
 
-os.system("kextunload /System/Library/Extensions/MobileFilterKext.kext")
-
 if os.path.exists("/System/Library/Extensions/MobileFilterKext.kext"):
+    os.system("kextunload /System/Library/Extensions/MobileFilterKext.kext")
     shutil.rmtree("/System/Library/Extensions/MobileFilterKext.kext")
 else:
     print("/System/Library/Extensions/MobileFilterKext.kext does not exist")
--- general_management_actions/uninstallLightspeedMobileFilter/uninstallLightspeedMobileFilter.sh
+++ general_management_actions/uninstallLightspeedMobileFilter/uninstallLightspeedMobileFilter.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-/usr/local/bin/mobilefilter -unload
-/bin/rm /usr/local/bin/mobilefilter
+if [ -a /usr/local/bin/mobilefilter ]
+  then /usr/local/bin/mobilefilter -unload; /bin/rm /usr/local/bin/mobilefilter
+  else "/usr/local/bin/mobilefilter does not exist"
+fi
 
-/bin/rm /Library/LaunchDaemons/com.lightspeedsystems.mobilefilter.plist
+if [ -a /Library/LaunchDaemons/com.lightspeedsystems.mobilefilter.plist ]
+  then /bin/rm /Library/LaunchDaemons/com.lightspeedsystems.mobilefilter.plist
+  else "/Library/LaunchDaemons/com.lightspeedsystems.mobilefilter.plist does not exist"
+fi
 
-kextunload /System/Library/Extensions/MobileFilterKext.kext
-/bin/rm -R /System/Library/Extensions/MobileFilterKext.kext
+if [ -a /System/Library/Extensions/MobileFilterKext.kext ]
+  then kextunload /System/Library/Extensions/MobileFilterKext.kext; /bin/rm -R /System/Library/Extensions/MobileFilterKext.kext
+  else "/System/Library/Extensions/MobileFilterKext.kext does not exist"
+fi
 
 #/usr/sbin/pkgutil --forget com.lightspeedsystems.mobilefilter.pkg
 
