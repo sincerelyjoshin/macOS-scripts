75ae5eb7c13f50040012d44e40b1c861a9e92f04
Josh Hovinetz <sincerelyjoshin@users.noreply.github.com>
2017-10-31 16:21:55 -0400

    Update disableFlashPlayerAutoUpdate.rb
    remove :create action, not necessary.
    added new line to content string

--- macOS/disableFlashPlayerAutoUpdate/disableFlashPlayerAutoUpdate.rb
+++ macOS/disableFlashPlayerAutoUpdate/disableFlashPlayerAutoUpdate.rb
@@ -1,6 +1,5 @@
 file '/Library/Application Support/Macromedia/mms.cfg' do
-    action  :create
-    content 'AutoUpdateDisable=1 SilentAutoUpdateEnable=0'
+    content "AutoUpdateDisable=1\nSilentAutoUpdateEnable=0"
     mode    '0755'
     owner    'root'
     group    'wheel'
