bfe507b7d7da6a11f493542aa7764be4b7950c53
Josh Hovinetz <sincerelyjoshin@users.noreply.github.com>
2024-02-05 13:39:09 -0500

    SMART Notebook
    - Script to query activation status

---
+++ macOS/SMART Notebook/querySMARTNotebookActivation.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -d /Applications/SMART\ Technologies/Notebook.app ]
+  then
+    ActivationStatus=$(/Applications/SMART\ Technologies/SMART\ Tools/SMART\ Product\ Update.app/Contents/MacOS/SMART\ Product\ Update --query [OPTIONS] --format xml --key "products.Notebook.licenseStatus" > /tmp/org.organization.smartstatus.plist 2> /dev/null; awk '{ print $1 }' /tmp/org.organization.smartstatus.plist)
+  else
+    ActivationStatus="Not Installed"
+fi
+
+if [ -z ActivationStatus ]
+  then
+    ActivationStatus="Activation Status Not Available"
+fi
+
+echo $ActivationStatus
+
+exit 0
